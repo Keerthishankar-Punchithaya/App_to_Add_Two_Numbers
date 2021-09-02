6ef380aa83d7415b64d6e3541fab2b5233b2ac4d
Keerthishankar Punchithaya <punchithayakeerthishankar@gmail.com>
2021-09-02 22:35:11 +0530

    Initial commit

---
+++ app/src/main/java/com/example/app_to_add_two_numbers/MainActivity.kt
@@ -0,0 +1,21 @@
+package com.example.app_to_add_two_numbers
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+
+
+        btnAdd.setOnClickListener {
+            val firstNumber = etFirstNumber.text.toString().toInt()
+            val secondNumber = etSecondNumber.text.toString().toInt()
+            val result = firstNumber + secondNumber
+            tvResult.text = result.toString()
+        }
+    }
+}
